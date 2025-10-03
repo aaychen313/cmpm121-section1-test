e778963dd38aa5c5b609732bc1a72542dc242180
aaychen313 <aaron111305@gmail.com>
2025-10-03 02:30:21 +0000

    fixed the counter

--- src/main.ts
+++ src/main.ts
@@ -22,5 +22,7 @@ const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
+  counter++;
+  counterElement.textContent = counter.toString();
   console.log("I have these thingies:", button, counterElement, counter);
 });
